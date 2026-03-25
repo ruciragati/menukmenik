79d63fa45fbf6d08dd101564c0b8180be3aa1b8b
Ruciragati Maheswari <ruciragati@Ruciragatis-MacBook-Air.local>
2026-03-26 00:49:57 +0100

    rotating issue

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    image = ImageOps.exif_transpose(image)
+    image = Image.new("RGB", img.size)
+    image.paste(img)
     st.image(image, caption='Uploaded Image', use_container_width=True)
     
     st.write("Identifying...")
