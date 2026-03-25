f054408ef2bf004af9e023b64cb71cef338f4e2c
guccirucci <96467384+ruciragati@users.noreply.github.com>
2026-03-26 00:23:36 +0100

    ui
    Added image upload functionality and preprocessing for model inference.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import torch
 import urllib.request
+from PIL import Image
+from torchvision import transforms
+from pillow_heif import register_heif_opener
 from src.model import get_dog_classifier
+
+register_heif_opener()
+
+st.title("Menuk or Menik")
+st.write("Upload a photo to see which Golden Girl it is!")
 
 @st.cache_resource
 def load_model():
     url = "https://huggingface.co/guccirucci/menukmenik/resolve/main/model.pth"
-    
     weights_path = "model.pth"
     urllib.request.urlretrieve(url, weights_path)
     
@@ -16,3 +23,32 @@ def load_model():
     return model
 
 model = load_model()
+
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+uploaded_file = st.file_uploader(
+        label="Upload Image", 
+        type=["jpg", "png", "jpeg"],
+        label_visibility="collapsed"  
+    )
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert('RGB')
+    st.image(image, caption='Uploaded Image', use_container_width=True)
+    
+    st.write("Identifying...")
+    input_tensor = preprocess(image).unsqueeze(0)
+    
+    with torch.no_grad():
+        output = model(input_tensor)
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+        confidence, prediction = torch.max(probabilities, 0)
+    
+    names = ["Menik", "Menuk"]
+    result = names[prediction.item()]
+    
+    st.success(f"That's **{result}**! ({confidence.item()*100:.2f}% confidence)")
