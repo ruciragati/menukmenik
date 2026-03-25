526d3103fed1d8299d7fc2a70a5491b9443f57e3
Ruciragati Maheswari <ruciragati@Ruciragatis-MacBook-Air.local>
2026-03-26 00:51:14 +0100

    final

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ uploaded_file = st.file_uploader(
     )
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert('RGB')
-    image = Image.new("RGB", img.size)
+    img = Image.open(uploaded_file)
+    image = Image.new("RGB", img.size) 
     image.paste(img)
     st.image(image, caption='Uploaded Image', use_container_width=True)
     
